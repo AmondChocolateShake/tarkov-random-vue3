3f31b110b82043abf1c2250f5858aae3a9551aad
KimEunA97 <kimeuna9712@gmail.com>
2023-11-19 22:46:58 +0900

    무기 컨테이너 배경색 변경

--- client/src/components/WeaponBox.js
+++ client/src/components/WeaponBox.js
@@ -4,7 +4,8 @@ export default function WeaponBox () {
   const WeaponBox = {
     width : "80vw",
     height: "80vh",
-    backgroundColor : "grey",
+    backgroundColor : "#1E1E1E",
+    border : "1px solid",
   }
   return (
     <div style={WeaponBox}></div>
