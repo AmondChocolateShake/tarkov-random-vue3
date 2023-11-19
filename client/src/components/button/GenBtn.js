6b45574ce96e229ac8b9d3309b1ae1b8af005007
KimEunA97 <kimeuna9712@gmail.com>
2023-11-20 01:37:06 +0900

    생성 버튼 마우스 오버 시 스타일링 변경 이벤트 추가

--- client/src/components/button/GenBtn.js
+++ client/src/components/button/GenBtn.js
@@ -1,4 +1,8 @@
+import { useState } from "react";
+
 export default function GenBtn() {
+  const [isHovering, setHovering] = useState(false);
+
   const btn = {
     width: "20%",
     height: "50%",
@@ -6,19 +10,48 @@ export default function GenBtn() {
     cursor: "pointer",
     borderRadius: 5,
     border: "2px solid white",
-    
+
     display: "flex",
     justifyContent: "space-evenly",
     alignItems: "center",
     color: "black",
-    fontWeight : "bold",
-    fontSize : 25
+    fontWeight: "bold",
+    fontSize: 30,
   };
-  const test = () => {
-    console.log("test");
+
+  const hoverdBtn = {
+    width: "20%",
+    height: "50%",
+    backgroundColor: "black",
+    cursor: "pointer",
+    borderRadius: 5,
+    border: "2px solid white",
+
+    display: "flex",
+    justifyContent: "space-evenly",
+    alignItems: "center",
+    color: "white",
+    fontWeight: "bold",
+    fontSize: 30,
+  };
+
+  const clicked = () => {
+    console.log("clicked");
+  };
+  const handleMouseDown = () => {
+    setHovering(true);
+  };
+  const handleMouseOver = () => {
+    setHovering(false);
+
   };
   return (
-    <div style={btn} onClick={test}>
+    <div
+      style={isHovering ? btn : hoverdBtn}
+      onClick={clicked}
+      onMouseOver={handleMouseOver}
+      onMouseLeave={handleMouseDown}
+    >
       Generate
     </div>
   );
