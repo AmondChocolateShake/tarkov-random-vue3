3722d1ae0adfd6ec747299a772a0a1439f971352
KimEunA97 <kimeuna9712@gmail.com>
2023-11-19 22:06:06 +0900

    무기 박스 정렬 변경

--- src/components/MainContainer.vue
+++ src/components/MainContainer.vue
@@ -1,84 +1,75 @@
 <template>
-    <div v-if="isLoading.value">...loading</div>
-    <div v-else>
-        <div class="main">
-            <div class="weapon">
-                <WeaponBox :weaponName = "weaponName" :imgLink= "imgLink"/>
-            </div>
-            <div class="itemListBox">
-                <!-- <ItemList :modList= "modSlots"/> -->
-            </div>
-        </div>
+  <div v-if="isLoading.value">...loading</div>
+  <div v-else>
+    <div class="main">
+      <div class="weapon">
+        <WeaponBox :weaponName="weaponName" :imgLink="imgLink" />
         <div class="generateBtnBox">
-            <GenBtn/>
+          <GenBtn />
         </div>
+      </div>
+      <div class="itemListBox">
+        <!-- <ItemList :modList= "modSlots"/> -->
+      </div>
     </div>
+  </div>
 </template>
 
 <script>
-    import { ref } from 'vue';
-    import WeaponBox from './WeaponBox.vue';
-    import GenBtn from './GenBtn.vue';
-    export default {
+import { ref } from "vue";
+import WeaponBox from "./WeaponBox.vue";
+import GenBtn from "./GenBtn.vue";
+export default {
+  components: {
+    WeaponBox,
+    GenBtn,
+  },
 
-        components:{
-            WeaponBox,
-            GenBtn,
-        },
+  setup() {
+    const weaponName = ref("AK-47");
+    const imgLink = ref(
+      "https://assets.tarkov.dev/5841474424597759ba49be91-image.webp"
+    );
+    // const modSlots=ref([]);
+    const isLoading = ref(true);
 
-        setup(){
-            const weaponName=ref('hel');
-            const imgLink=ref('https://assets.tarkov.dev/5841474424597759ba49be91-image.webp');
-            // const modSlots=ref([]);
-            const isLoading=ref(true);
-            
-            return {
-                weaponName,
-                imgLink,
-                isLoading
-            };
-        }
-    }
-    
+    return {
+      weaponName,
+      imgLink,
+      isLoading,
+    };
+  },
+};
 </script>
 
 <style>
-    .main{
+.main {
+  width: 80vw;
+  height: 70vh;
+  background-color: #1e1e1e;
+  border-radius: 15px;
+  border: 2px solid rgba(255, 255, 255, 80%);
+}
 
-        width: 500px;
-        height: 600px;
-        background-color: #1e1e1e;
-        border-radius: 15px;
-        border:2px solid rgba(255, 255, 255, 80%);
-    }
+.generateBtnBox {
+}
 
-    .generateBtnBox{
-        display: flex;
-        justify-content: flex-end;
-        align-items: center;
-        
+.weapon {
+  display: flex;
+  justify-content: space-around;
+  align-items: center;
+  width: 100%;
+  height: 200px;
+  border-bottom: 3px solid rgba(255, 255, 255, 80%);
+}
 
-        width:100%,
-        
-    }
+.itemListBox {
+  display: flex;
+  justify-content: center;
+  align-items: center;
 
-    .weapon{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-        width: 100%;
-        height: 200px;
-        border-bottom: 3px solid rgba(255,255,255,80%);
-    }
-
-    .itemListBox{
-        display: flex;
-        justify-content: center;
-        align-items: center;
-
-        width: 100%;
-        height:400px;
-        min-height: 400px;
-    }
-
+  width: 100%;
+  height: 400px;
+  min-height: 400px;
+}
 </style>
