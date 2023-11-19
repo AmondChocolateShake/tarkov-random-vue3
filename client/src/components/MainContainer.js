9e4424b9165e316ecfb53550859c1a721e384047
KimEunA97 <kimeuna9712@gmail.com>
2023-11-19 22:42:48 +0900

    Logo 컴포넌트 추가

--- client/src/components/MainContainer.js
+++ client/src/components/MainContainer.js
@@ -1,15 +1,20 @@
 import React from "react";
+import TarkovLogo from "./TarkovLogo";
 
 export default function MainContainer() {
   const container = {
     width: "100vw",
-    height : "100vh",
+    height: "100vh",
     color: "white",
     background: "black",
-    display : "flex",
-    flexDirection : "row",
-    justifyContent : "center",
-    alignItems : "center",
+    display: "flex",
+    flexDirection: "row",
+    justifyContent: "center",
+    alignItems: "center",
   };
-  return <div style={container}>mainBox</div>;
+  return (
+    <div style={container}>
+      <TarkovLogo/>
+    </div>
+  );
 }
---
+++ client/src/components/TarkovLogo.js
@@ -0,0 +1,7 @@
+import React from "react"
+
+export default function TarkovLogo () {
+  return (
+    <div>TarkovLogo</div>
+  )
+}
