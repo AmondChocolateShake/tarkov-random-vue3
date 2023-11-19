d39218b9685767cc0af908bac17089a0d3338a21
KimEunA97 <kimeuna9712@gmail.com>
2023-11-20 00:24:29 +0900

    무기 박스 스타일링 // 이미지 컨테이너, 폰트 크기 변경

--- client/src/components/weapon/WeaponImg.js
+++ client/src/components/weapon/WeaponImg.js
@@ -1,14 +1,34 @@
+import data from "../../dummy.json";
+
 export default function WeaponImg() {
-  const img = {
+  const imgBox = {
     width: "60%",
     height: "70%",
     backgroundColor: "rgba(158, 158, 158, 1)",
     border: "2px solid white",
     borderRadius: 5,
 
-    display : "flex",
-    justifyContent : "center",
-    alignItems : "center",
+    display: "flex",
+    justifyContent: "center",
+    alignItems: "center",
   };
-  return <div style={img}>WeaponImg</div>;
+
+  const img = {
+    width: "100%",
+    height: "100%",
+    objectFit: "cover",
+    borderRight: "2px solid white",
+  };
+  const name = {
+    textAlign: "center",
+    color: "white",
+    fontWeight: "bold",
+    fontSize : 30,
+  };
+  return (
+    <div style={imgBox}>
+      <img style={img} src={data.imgLink} overflow alt="weaponImg" />
+      <div style={name}>{data.name}</div>
+    </div>
+  );
 }
