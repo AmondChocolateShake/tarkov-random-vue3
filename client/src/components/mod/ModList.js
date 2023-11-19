7d8c4774f160f5573de38ac0d13179a836ab1187
KimEunA97 <kimeuna9712@gmail.com>
2023-11-20 01:56:47 +0900

    리스트 스타일 비율 조절중

--- client/src/components/mod/ModList.js
+++ client/src/components/mod/ModList.js
@@ -8,23 +8,32 @@ export default function ModList() {
     height: "100%",
     width : "100%",
     flexWrap : "wrap",
-    overflow : "scroll"
+    overflow : "scroll",
   }
   const list = {
-    width: "30%",
+    width: "40%",
     height: "20%",
     backgroundColor: "rgba(30, 30, 30, 1)",
     margin: 20,
     display : "flex",
     flexDirection : "row",
+    justifyContent: "center",
     borderRadius : 5,
+    border : "1px solid white",
   };
   const modImg = {
+    width : "100%",
+    height : "100%",
+    borderRight : "1px solid white",
     display: "flex",
-    objectFit: "cover",
+    objectFit: "contain",
   };
   const text = {
+    display : "flex",
+    alignItems : "center",
     color: "white",
+    textAlign : "center",
+    margin : 5
   };
   return (
     <div style={container}>
