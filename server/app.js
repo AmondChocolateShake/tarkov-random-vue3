69c12b1fe07f24a6ca161ed17428dbd2f3fa7e52
KimEunA97 <kimeuna9712@gmail.com>
2023-11-20 02:31:55 +0900

    서버 셋업

--- server/app.js
+++ server/app.js
@@ -1,13 +1,10 @@
 const express = require("express");
 const app = express();
-const port = 3000;
+const port = 5000;
 
 app.get("/", (req, res) => {
-  res.send('hellso')
-
+  res.send("hellso");
 });
-
-
-app.listen(port,()=>{
-  console.log("server working")
-})
+app.listen(port, () => {
+  console.log("server working");
+});
