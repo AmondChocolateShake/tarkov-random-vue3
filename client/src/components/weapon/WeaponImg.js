d5fec2ea9f3245d2c56d60e71d601a7f89b1ab29
KimEunA97 <kimeuna9712@gmail.com>
2023-11-19 23:57:25 +0900

    무기 박스 스타일링

--- client/src/components/weapon/WeaponImg.js
+++ client/src/components/weapon/WeaponImg.js
@@ -1,11 +1,14 @@
 export default function WeaponImg() {
-  
-  const img= {
-    width : "70%",
-    height : "80%",
-    backgroundColor : "rgba(158, 158, 158, 1)",
-  }
-  return (
-    <div style={img}>WeaponImg</div>
-  )
+  const img = {
+    width: "60%",
+    height: "70%",
+    backgroundColor: "rgba(158, 158, 158, 1)",
+    border: "2px solid white",
+    borderRadius: 5,
+
+    display : "flex",
+    justifyContent : "center",
+    alignItems : "center",
+  };
+  return <div style={img}>WeaponImg</div>;
 }
