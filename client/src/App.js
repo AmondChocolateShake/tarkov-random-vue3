6d13e9a409dd3bd9863e182c314b69f99c36d3f3
KimEunA97 <kimeuna9712@gmail.com>
2023-11-20 04:28:34 +0900

    ThemeProvider 바인딩

--- client/src/App.js
+++ client/src/App.js
@@ -6,7 +6,7 @@ function App() {
   return (
     <div className="App">
       <ThemeProvider theme={theme}>
-        <MainContainer />
+          <MainContainer />
       </ThemeProvider>
     </div>
   );
