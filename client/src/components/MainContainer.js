b0ebbfd200338fd209e49b1da7041f73a96ef12c
KimEunA97 <kimeuna9712@gmail.com>
2023-11-21 13:00:50 +0900

    스타일드 선언 컴포넌트 외부로 옮기기 (리렌더링 발생 때문

--- client/src/components/MainContainer.js
+++ client/src/components/MainContainer.js
@@ -3,17 +3,18 @@ import TarkovLogo from "./logo/TarkovLogo";
 import InnerContainer from "./InnerContainer";
 import styled from "styled-components";
 
+const StyledDiv = styled.div`
+  width: 100vw;
+  height: 100vh;
+  color: white;
+  background: black;
+  display: flex;
+  flex-direction: column;
+  justify-content: center;
+  align-items: center;
+`;
+
 export default function MainContainer() {
-  const StyledDiv = styled.div`
-    width: 100vw;
-    height: 100vh;
-    color: white;
-    background: black;
-    display: flex;
-    flex-direction: column;
-    justify-content: center;
-    align-items: center;
-  `;
   return (
     <StyledDiv>
       <TarkovLogo />
--- client/src/components/load/LoadComponent.js
+++ client/src/components/load/LoadComponent.js
@@ -1,48 +1,48 @@
 import styled from "styled-components";
 
-export default function LoadComponent() {
-  const LoadContainer = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: inherit;
-    height: inherit;
-    background-color: rgba(30, 30, 30, 1);
-    border-radius: 5px;
-  `;
-  const LoadTopSty = styled.div`
-    display: flex;
-    flex-direction: row;
-    align-items: center;
-    justify-content: center;
-    width: 100%;
-    height: 20%;
-    background-color: rgba(30, 30, 30, 1);
-    border-radius: 5px;
-  `;
-  const LoadTopDivSty = styled.div`
-    width: 90%;
-    height: 80%;
-    background-color: rgba(0, 0, 0, 0.8);
-    border-radius: 5px;
-  `;
+const LoadContainer = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: inherit;
+  height: inherit;
+  background-color: rgba(30, 30, 30, 1);
+  border-radius: 5px;
+`;
+const LoadTopSty = styled.div`
+  display: flex;
+  flex-direction: row;
+  align-items: center;
+  justify-content: center;
+  width: 100%;
+  height: 20%;
+  background-color: rgba(30, 30, 30, 1);
+  border-radius: 5px;
+`;
+const LoadTopDivSty = styled.div`
+  width: 90%;
+  height: 80%;
+  background-color: rgba(0, 0, 0, 0.8);
+  border-radius: 5px;
+`;
+const LoadBottomSty = styled.div`
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  width: 100%;
+  height: 80%;
+  background-color: rgba(0, 0, 0, 1);
+  border-radius: 5px;
+`;
+const LoadBottomDivSty = styled.div`
+  margin-top: 10px;
+  width: 95%;
+  height: 20%;
+  background-color: rgba(30, 30, 30, 1);
+  border-radius: 5px;
+`;
 
-  const LoadBottomSty = styled.div`
-    display: flex;
-    flex-direction: column;
-    align-items: center;
-    width: 100%;
-    height: 80%;
-    background-color: rgba(0, 0, 0, 1);
-    border-radius: 5px;
-  `;
-  const LoadBottomDivSty = styled.div`
-    margin-top: 10px;
-    width: 95%;
-    height: 20%;
-    background-color: rgba(30, 30, 30, 1);
-    border-radius: 5px;
-  `;
+export default function LoadComponent() {
 
   return (
     <LoadContainer>
