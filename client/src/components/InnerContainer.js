7ef7888e5ddfd5e5323abca9343095e3630890aa
KimEunA97 <kimeuna9712@gmail.com>
2023-11-21 12:21:31 +0900

    재요청 구현중

--- client/src/components/InnerContainer.js
+++ client/src/components/InnerContainer.js
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from "react";
 import ModBox from "./mod/ModBox";
 import WeaponBox from "./weapon/WeaponBox";
 import LoadComponent from "./load/LoadComponent";
-import fetchData from './fetchData.js'
+import fetchData from "./fetchData.js";
 
 export const UserContext = React.createContext();
 
@@ -11,20 +11,31 @@ export default function InnerContainer() {
   const [loading, setLoading] = useState(true);
 
   //fetch 재요청 함수
-  const fetchReq = () =>{
-    fetchData();
-  }
+  const fetchReq = () => {
+    getFetchData();
+  };
+
+  //get 요청
+  const getFetchData = async () => {
+    try {
+      const fetchedData = await fetchData();
+      setData(fetchedData);
+      setLoading(false);
+    } catch (error) {
+      console.error("Error fetching Data! :", error);
+    }
+  };
 
   useEffect(() => {
     if (loading) {
-      setData(fetchData());
+      getFetchData();
     }
-  }, []);
+  }, [loading]);
 
   const InnerContainer = {
     width: "80vw",
     height: "80vh",
-    maxWidth : "800px",
+    maxWidth: "800px",
     backgroundColor: "#1E1E1E",
     border: "1px solid",
     borderRadius: "5px",
@@ -33,12 +44,10 @@ export default function InnerContainer() {
     flexDirection: "column",
   };
 
-  
-
   return (
     <div style={InnerContainer}>
       {loading ? (
-        <LoadComponent/>
+        <LoadComponent />
       ) : (
         <UserContext.Provider value={data}>
           <WeaponBox callbackClicked={fetchReq} />
