b7e1c859b13d0c7a2cccbd9bd02540e77156e660
KimEunA97 <kimeuna9712@gmail.com>
2023-11-28 10:17:23 +0900

    생성 버튼 스타일 => 스타일드 컴포넌트로 변경

--- tsclient/src/components/button/GenBtn.tsx
+++ tsclient/src/components/button/GenBtn.tsx
@@ -1,52 +1,57 @@
 import { useState } from "react";
+import styled from "styled-components";
 
-export default function GenBtn({ callbackClicked }) {
+const Btn = styled.div`
+width: "20%";
+height: "50%";
+backgroundColor: "rgba(217, 217, 217, 1)";
+cursor: "pointer";
+borderRadius: 5;
+border: "3px solid white";
+
+textAlign: "center";
+wordBreak: "break-all";
+display: "flex";
+justifyContent: "space-evenly";
+alignItems: "center";
+color: "rgba(0,0,0, 0.8)";
+fontWeight: "bold";
+fontSize: 30;
+'@media (max-width:600px)': {
+  fontSize: 10;
+},
+`;
+const HoverdBtn = styled.div`
+width: "20%";
+height: "50%";
+backgroundColor: "black";
+borderRadius: 5;
+border: "3px solid white";
+
+  text-align: "center";
+  word-break: "break-all";
+  display: "flex";
+  justify-content: "space-evenly";
+  align-items: "center";
+  color: "rgba(255,255,255, 0.7)";
+  font-weight: "bold";
+  font-size: 30;
+  '@media (max-width:600px)': {
+    font-size: 10;
+  },
+`;
+const Gentext = styled.div`
+  font-family: "bendereglarbold";
+`;
+
+export default function GenBtn({
+  callbackClicked,
+}: {
+  callbackClicked: () => void;
+}) {
   const [isHovering, setHovering] = useState(false);
   // const [isClicked, setClicked] = useState(false);
 
-  const btn = {
-    width: "20%",
-    height: "50%",
-    backgroundColor: "rgba(217, 217, 217, 1)",
-    cursor: "pointer",
-    borderRadius: 5,
-    border: "3px solid white",
-
-    textAlign: "center",
-    wordBreak: "break-all",
-    display: "flex",
-    justifyContent: "space-evenly",
-    alignItems: "center",
-    color: "rgba(0,0,0, 0.8)",
-    fontWeight: "bold",
-    fontSize: 30,
-        '@media (max-width:600px)': {
-      fontSize: 10,
-    },
-  };
-
-  const hoverdBtn = {
-    width: "20%",
-    height: "50%",
-    backgroundColor: "black",
-    borderRadius: 5,
-    border: "3px solid white",
-
-    textAlign: "center",
-    wordBreak: "break-all",
-    display: "flex",
-    justifyContent: "space-evenly",
-    alignItems: "center",
-    color: "rgba(255,255,255, 0.7)",
-    fontWeight: "bold",
-    fontSize: 30,
-    '@media (max-width:600px)': {
-      fontSize: 10,
-    },
-  };
-  const gentext = {
-    fontFamily: "bendereglarbold",
-  };
   const clicked = () => {
     callbackClicked();
     setHovering(true);
@@ -57,8 +62,9 @@ export default function GenBtn({ callbackClicked }) {
   };
 
   return (
-    <div style={isHovering ? hoverdBtn : btn} onClick={clicked}>
-      <div style={gentext}>Generate</div>
+    <div>
+      {isHovering ? <HoverdBtn /> : <Btn onClick={clicked} />}
+      <Gentext>Generate</Gentext>
     </div>
   );
 }
